008da955c4a990cb8ae5ad157771201b9bb73ce9
lsh <likey1121@snu.ac.kr>
2021-11-21 23:31:06 +0900

    modified:   kernel/rotation.c

--- kernel/rotation.c
+++ kernel/rotation.c
@@ -5,12 +5,22 @@
 #include <linux/list.h> // kernel list API
 #include <mutex.h>
 
-static struct rotlock_t read_waiting;
+static int rotation = 0;
+
+static LIST_HEAD(read_waiting);
+static LIST_HEAD(write_waiting);
+static LIST_HEAD(read_active);
+static LIST_HEAD(write_active);
+
+static DEFINE_MUTEX(rotlock_mutex);
 
 rotlock_t* init_rotlock(int degree, int range, int rw_type) 
 {
     rotlock_t* rotlock;
     rotlock = (rotlock_t *)kmalloc(sizeof(rotlock_t));
+    if(!rotlock)
+        return NULL;
+    rotlock.pid = getpid();
     rotlock.degree = degree;
     rotlock.range = range;
     rotlock.rw_type = rw_type;
@@ -21,20 +31,101 @@ rotlock_t* init_rotlock(int degree, int range, int rw_type)
 
 SYSCALL_DEFINE1(set_rotation, int, degree)
 {
+    if(degree < 0 || degree >= 360) {
+        printk(KERN_ERR "Degree argument should be between 0 and 360\n");
+        return -EINVAL;
+    }
+
+    rotation = degree;
 }
+
+// We should check the condition degree - range <= LOCK RANGE <= degree + range
 
 SYSCALL_DEFINE2(rotlock_read, int, degree, int, range)
 {
+    rotlock_t *rotlock;
+    int cnt;
+
+    if(degree < 0 || degree >= 360) {
+        printk(KERN_ERR "degree should be 0 <= degree < 360\n");
+        return -EINVAL;
+    }
+
+    if(range <= 0 || range >= 180) {
+        printk(KERN_ERR "range should be 0 < range < 180\n");
+        return -EINVAL;
+    }
+
+    rotlock = init_rotlock(degree, range, READ);
+
+    if(!rotlock) {
+        printk(KERN_ERR "kmalloc failed\n");
+        return -ENOMEM;
+    }
+
+    return -1;
 }
 
 SYSCALL_DEFINE2(rotlock_write, int, degree, int, range)
 {
+    rotlock_t *rotlock;
+    int cnt;
+
+    if(degree < 0 || degree >= 360) {
+  	    printk(KERN_ERR "degree should be 0 <= degree < 360\n");
+        return -EINVAL;
+    }
+
+    if(range <= 0 || range >= 180) {
+        printk(KERN_ERR "range should be 0 < range < 180\n");
+        return -EINVAL;
+    }
+
+    rotlock = init_rotlock(degree, range, WRITE);
+    
+    if(!rotlock) {
+        printk(KERN_ERR "kmalloc failed\n");
+        return -ENOMEM;
+    }
+    return -1;
 }
 
 SYSCALL_DEFINE2(rotunlock_read, int, degree, int, range)
 {
+    rotlock_t *rotlock;
+    int cnt;
+
+    if(degree < 0 || degree >= 360) {
+  	    printk(KERN_ERR "degree should be 0 <= degree < 360\n");
+        return -EINVAL;
+    }
+
+    if(range <= 0 || range >= 180) {
+        printk(KERN_ERR "range should be 0 < range < 180\n");
+        return -EINVAL;
+    }
+
+    //rotlock = init_rotlock(degree, range, READ);
+
+    return -1;
 }
 
 SYSCALL_DEFINE2(rotunlock_write, int, degree, int, range)
 {
+    rotlock_t *rotlock;
+    int cnt;
+
+    if(degree < 0 || degree >= 360) {
+  	    printk(KERN_ERR "degree should be 0 <= degree < 360\n");
+        return -EINVAL;
+    }
+
+    if(range <= 0 || range >= 180) {
+        printk(KERN_ERR "range should be 0 < range < 180\n");
+        return -EINVAL;
+    }
+
+    //rotlock = init_rotlock(degree, range, WRITE);
+
+    return -1;
 }
